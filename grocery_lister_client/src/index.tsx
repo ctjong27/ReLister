976186a5e3720e38634b1a73d74a7663ca4f1a8f
acer_laptop <cjong93.login@gmail.com>
2020-06-07 22:04:40 -0400

    init - index to app routing to HomePage working
    Minimal development done, finished setting up basic routing to get to home page.
    
    Will need to work on cleaning up & adding more logic to have prettier layout.

--- grocery_lister_client/src/index.tsx
+++ grocery_lister_client/src/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import {createBrowserHistory } from 'history';
+import * as serviceWorker from "./archive/serviceWorker";
+
+import App from "./app/layout/App";
+import ScrollToTop from "./app/layout/ScrollToTop";
+import "./index.css";
+
+export const history = createBrowserHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router history={history}>
+    <ScrollToTop>
+      <div>
+        <App />
+      </div>
+    </ScrollToTop>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
