f5e0376e5894a345c9ffa230f9976e0cb319cd8f
acer_laptop <cjong93.login@gmail.com>
2020-07-02 20:12:16 -0400

    Done: Password security encryption complete

--- relister_server/source/models/user.py
+++ relister_server/source/models/user.py
@@ -1,13 +1,15 @@
 import sqlite3
 from source.db import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class UserModel(db.Model):
 
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    password = db.Column(db.String(80))
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    # password = db.Column(db.String(80))
+    password = db.Column(db.String(300), nullable=False)
 
     # Linked Foreign Key
     ingredients = db.relationship('IngredientModel', lazy='dynamic')
@@ -17,11 +19,18 @@ class UserModel(db.Model):
         self.username = username
         self.password = password
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
     def json(self):
         # return {'username':self.username, 'ingredients':[ingredient.json() for item in self.ingredients.all()]} # ingredients.all() is because relationship is lazy=dynamic
         return {'username':self.username, 'recipes':[recipe.json() for recipe in self.recipes.all()]} # recipes.all() is because relationship is lazy=dynamic
 
     def save_to_db(self):
+        self.set_password(self.password)
         db.session.add(self)
         db.session.commit()
 
